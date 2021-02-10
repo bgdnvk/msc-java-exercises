310f8e5725e7ab8c33684b428d10541a348d1288
bgdnvk <bnovykov@gmail.com>
2021-02-10 04:16:42 +0100

    .

---
+++ ej2/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.*;
+
+class Driver {
+    public static void main(String[] arg) {
+		/* Asignamos al tipo leyenda true porque queremos que nos pinte la
+		   cadena Garaje vac�o que es lo primero que vamos a mostrar*/
+        boolean leyenda = true;
+
+		/* Inicializa un garaje de asignando colores de forma aleatoria
+		   Debe tener cada planta todos lo colores*/
+        Garaje g = new Garaje();
+
+        // Mostramos la asignacion de colores a las plazas con el garaje vac�o
+        g.pintarGaraje(leyenda);
+
+		/* Si no caben m�s devuelve el valor de los coches que no ha podido
+		   aparcar
+		   Si devuelve un valor negativo (-1) es que el random ha sido 0, ning�n
+		   coche a aparcar
+		   Calculo el n�mero de veh�culos a entrar de forma aleatoria*/
+        Random random = new Random();
+        int aleatorio = random.nextInt(100);
+        int sobra = g.entradaCoches(aleatorio);
+        leyenda = false;
+
+        if (aleatorio > 0) {
+            // Entra m�s de un coche
+            System.out.println("Resultado de veh�culos aparcados:\n");
+            g.pintarGaraje(leyenda);
+            System.out.println("Se han intentado aparcar " + aleatorio
+                    + " coches.");
+            if (sobra != 0) {
+                // Han sobrado coches.
+                System.out.println("No han cabido " + sobra + " coches");
+            }
+
+        } else {
+            System.out.println("No hay coches");
+        }
+    }
+}

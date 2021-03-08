5ce92b4aed6de2f3359e4c20863881fa439d099e
bgdnvk <bnovykov@gmail.com>
2021-03-08 21:19:06 +0100

    db connection practice

---
+++ jdbc/src/main/java/main.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class main {
+    public static void main(String[] args) {
+        String URL = "jdbc:mysql://localhost:3306/my_schema";
+        String username = "root";
+        String pass= "password";
+        String query = "SELECT * FROM my_schema.comercios";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, username, pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                String comercioData = "";
+                for (int i = 1; i <= 3; i++){
+                    comercioData+=resultSet.getString(i) + ";";
+                }
+                System.out.println(comercioData);
+            }
+
+
+        } catch (SQLDataException e){
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}

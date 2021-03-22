3ab10f6fca266306aa4dd1d9843915a0a9bc585b
bgdnvk <bnovykov@gmail.com>
2021-03-22 18:45:16 +0100

    createStringExcel works as intended

--- hope/src/main/java/CreateExcel.java
+++ hope/src/main/java/CreateExcel.java
@@ -37,61 +37,28 @@ public class CreateExcel {
             workbook.write(outputStream);
         }
     }
+    //create an excel that spreads data into two cells per row
     public static void createStringExcel(ArrayList<String> arrayList) throws IOException{
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Java Data");
         //to start from 0 since it updates in the loop
         int rows = -1;
 
-//        for (String dataString: arrayList){
-//            //creates a new row every time for the loop
-//            Row row = sheet.createRow(++rows);
-//            //display what site you are scrapping
-//            System.out.println("cell is"+ dataString);
-//
-//            //make a new cell with the scrapped info
-//            setCell(row, dataString);
-//        }
-        for (int i = 0; i < arrayList.size(); i++){
+        //System.out.println(arrayList.size());
+        //the input has continous rows, we need to break it
+        //every row has 2 cells, so we increase by 2 and make 2 cells per row
+        for (int i = 0; i < arrayList.size(); i+=2){
+            //check i
+            //System.out.println("i is: "+i);
 
-            Row row;
-
-//            if(i%2 != 0){
-//                System.out.println("I is: "+i);
-//                row = sheet.createRow(rows);
-//                Cell cell = row.createCell(1);
-//                System.out.println(arrayList.get(i));
-//
-//                cell.setCellValue(arrayList.get(i));
-//            } else {
-//                System.out.println("does it ever get here?????????????");
-//                row = sheet.createRow(++rows);
-//                System.out.println("rows is "+rows);
-//                //setCell(row, arrayList.get(i));
-//                Cell cell = row.createCell(0);
-//                System.out.println(arrayList.get(i));
-//                cell.setCellValue(arrayList.get(i));
-//            }
-
-            //TODO fix this awful thing
-            int cellId;
-            if (i%2 != 0){
-
-                cellId = 1;
-
-            } else {
-                cellId = 0;
-                rows = ++rows;
-            }
-
-            row = sheet.createRow(rows);
-            System.out.println("rows is "+rows);
-            //setCell(row, arrayList.get(i));
-            Cell cell = row.createCell(cellId);
-            System.out.println(arrayList.get(i));
-            cell.setCellValue(arrayList.get(i));
-
-
+            //make a new row every time the loop is called
+            Row row = sheet.createRow(++rows);
+            //make the two cells
+            Cell cell0 = row.createCell(0);
+            Cell cell1 = row.createCell(1);
+            //add data to the two cells, then continue
+            cell0.setCellValue(arrayList.get(i));
+            cell1.setCellValue(arrayList.get(i+1));
 
         }
 
